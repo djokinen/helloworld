10cf5876262d6184bba3687d24d660662be8f948
Dan <djokinen@ryerson.ca>
2013-12-10 12:15:25 -0500

    create project
    created the hello world project

---
+++ script/core.js
@@ -0,0 +1,7 @@
+﻿$(function () {
+	// alert(tinymce.content);
+	// var data = "this is my data";
+	// tinymce.get('textarea.longdesc').setContent(data);
+	var tinymce = $("textarea.tinymce");
+	tinymce.val("hello world!");
+});
