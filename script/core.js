b338bdff5cc57ce55103509c06e54c00f71a692a
Dan <djokinen@ryerson.ca>
2013-12-10 12:57:48 -0500

    write value in uppercase

--- script/core.js
+++ script/core.js
@@ -3,5 +3,5 @@
 	// var data = "this is my data";
 	// tinymce.get('textarea.longdesc').setContent(data);
 	var tinymce = $("textarea.tinymce");
-	tinymce.val("hello world!");
+	tinymce.val("hello world!".toUpperCase());
 });
