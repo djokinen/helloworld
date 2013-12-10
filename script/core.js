fcf5f9f8ac8866d0f20b287e1d174f6831012c08
Dan <djokinen@ryerson.ca>
2013-12-10 13:00:08 -0500

    remove useless code

--- script/core.js
+++ script/core.js
@@ -1,7 +1,4 @@
 ﻿$(function () {
-	// alert(tinymce.content);
-	// var data = "this is my data";
-	// tinymce.get('textarea.longdesc').setContent(data);
 	var tinymce = $("textarea.tinymce");
 	tinymce.val("hello world!".toUpperCase());
 });
